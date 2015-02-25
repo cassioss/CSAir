21642d9600f5f56f018c07f6a1afb485647f8f7b
Cassio Sousa <cassiosantossousa@gmail.com>
2015-02-25 16:52:39 -0600

    Add to_s method to Node

--- lib/graph/node.rb
+++ lib/graph/node.rb
@@ -8,9 +8,34 @@ class Node
     @connections = Hash.new
   end
 
-  def to_s
-    puts "Node{'#{@airport}'}"
+  def add_comma_to(my_string)
+    my_string << ','
   end
+
+  def add_connections_to_string
+    all_connections = String.new
+    add_comma = false
+    @connections.each do |port, dist|
+      if add_comma
+        add_comma_to(all_connections)
+      else
+        add_comma = true
+      end
+      all_connections << " {'#{port}' => #{dist}}"
+    end
+    all_connections
+  end
+
+  # Returns a string containing the airpot name and all its
+  # connections, if any.
+  def to_s
+    airport_string = "Node{'#{@airport}'}"
+    unless @connections.empty?
+      airport_string << ':' << add_connections_to_string
+    end
+    airport_string
+  end
+
   # @param [String] port
   # @param [Integer] distance
   def add_connection(port, distance)
@@ -21,15 +46,12 @@ class Node
   # @return [Integer]
   def distance_to(airport)
     if airport == @airport
-      return 0
+      0
     elsif !@connections.include?(airport)
-      return 1.0/0.0
+      1.0/0.0
     else
-      return @connections[airport]
+      @connections[airport]
     end
   end
-
-  a = Node.new('MEX')
-  p a
 
 end
