edd6d9851d3f1afa3635b079194489c3f5951fef
Cassio Sousa <cassiosantossousa@gmail.com>
2015-02-25 20:50:17 -0600

    Structured Graph as a double hash

--- lib/graph/graph.rb
+++ lib/graph/graph.rb
@@ -1,20 +1,49 @@
-require_relative 'node'
-
 class Graph
-  def initialize
-    @node_set = Set.new
-  end
+  INFTY = 1.0/0.0
 
-  # @param [String] port_name
-  def add_node(port_name)
-    @node_set << Node.new(port_name)
+  def initialize
+    @node_hash = Hash.new
   end
 
   # @param [String] first_port
   # @param [String] second_port
   # @param [Integer] distance
   def add_connection(first_port, second_port, distance)
-
+    check_existence_of(first_port)
+    check_existence_of(second_port)
+    @node_hash[first_port][second_port] = distance
+    @node_hash[second_port][first_port] = distance
   end
+
+  # @param [String] first_port
+  # @param [String] second_port
+  # @return [Integer]
+  def get_connection(first_port, second_port)
+    if one_does_not_exist(first_port, second_port)
+      -1
+    else
+      @node_hash[first_port][second_port]
+    end
+  end
+
+  private
+  # @param [String] first_port
+  # @param [String] second_port
+  def one_does_not_exist(first_port, second_port)
+    !@node_hash.include? first_port or !@node_hash.include? second_port
+  end
+
+  # @param [String] port_name
+  def add_node(port_name)
+    @node_hash[port_name] = Hash.new(INFTY)
+  end
+
+  # @param [String] port
+  def check_existence_of(port)
+    unless @node_hash.include? port
+      add_node(port)
+    end
+  end
+
 
 end
--- spec/graph/graph_test.rb
+++ spec/graph/graph_test.rb
@@ -1,23 +1,33 @@
+require_relative '../../lib/graph/graph'
 require 'test/unit'
 
 class GraphTest < Test::Unit::TestCase
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
-  def setup
-    # Do nothing
+  INFTY = 1.0/0.0
+
+  # Tests if the insertion of connection between ports is bilateral.
+  def test_add_connection
+    simple_graph = Graph.new
+    simple_graph.add_connection('MEX', 'CHI', 2131)
+    assert_equal(simple_graph.get_connection('MEX', 'CHI'), 2131)
+    assert_equal(simple_graph.get_connection('CHI', 'MEX'), 2131)
   end
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
-
-  def teardown
-    # Do nothing
+  # Tests getting the connection of an airport that does not exist.
+  def test_get_non_existing_port
+    simple_graph = Graph.new
+    simple_graph.add_connection('MEX', 'CHI', 3141)
+    assert_equal(simple_graph.get_connection('MEX', 'ABC'), -1)
+    assert_equal(simple_graph.get_connection('ABC', 'MEX'), -1)
   end
 
-  # Fake test
-  def test_fail
-
-    fail('Not implemented')
+  # Tests getting the connection of two existing but disconnected airports.
+  def test_get_non_existing_port
+    simple_graph = Graph.new
+    simple_graph.add_connection('MEX', 'CHI', 3141)
+    assert_equal(simple_graph.get_connection('MEX', 'ABC'), -1)
+    assert_equal(simple_graph.get_connection('ABC', 'MEX'), -1)
   end
+
+
 end
