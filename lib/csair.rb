1a99d94392e374354578eb3ef793f3527d51b04d
Cassio Sousa <cassiosantossousa@gmail.com>
2015-03-12 10:41:44 -0500

    Began saving changes on a JSON file

--- lib/csair.rb
+++ lib/csair.rb
@@ -1,5 +1,6 @@
 require 'csair/version'
 require_relative '../lib/cli/cmd_interface'
+require 'json'
 
 # Module that initiates the Command Line Interface (CLI) as a for loop.
 #
@@ -9,4 +10,16 @@ require_relative '../lib/cli/cmd_interface'
 module CSAir
   cli = CmdInterface.new
   cli.basic_for_loop
+
+  tempHash = {
+      :key_a => 10,
+      :key_b => 20,
+  }
+
+  my_path = File.dirname(__FILE__)
+  path_to_json = File.join(my_path, '..', 'resources', 'new_map_data.json')
+  File.open(path_to_json, 'w') do |f|
+    f.write(tempHash.to_json)
+  end
+
 end
